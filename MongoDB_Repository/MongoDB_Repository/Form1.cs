c83e44ff72b52e2b0461f378b21fc55b420f2196
DusanM998 <75371025+DusanM998@users.noreply.github.com>
2022-03-14 10:46:03 +0100

    Add files via upload

---
+++ MongoDB_Repository/MongoDB_Repository/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+
+
+namespace MongoDB_Repository
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var database = server.GetDatabase("preduzece");
+            
+            var collection = database.GetCollection<Radnik>("radnici");
+
+            MongoCursor<Radnik> radnici = collection.FindAll();
+
+            foreach (Radnik r in radnici.ToArray<Radnik>())
+            {
+                MessageBox.Show(r.ime);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var db = server.GetDatabase("preduzece");
+
+            //db.CreateCollection("preduzece");
+
+            var collection = db.GetCollection<Radnik>("radnici");
+
+            Radnik radnik1 = new Radnik { ime = "Petar", prezime = "Petrović", Adresa = "Oblačića Rada 12", Plata = 25000, oznake = new List<string> { "muškarac", "ekonomista", "pripravnik" } };
+            Radnik radnik2 = new Radnik { ime = "Sava", prezime = "Janković", Adresa = "Voždova 23", Plata = 40000, oznake = new List<string> { "muškarac", "pravnik", "rukovodilac" } };
+            Radnik radnik3 = new Radnik { ime = "Milutin", prezime = "Simić", Adresa = "Dušanova 112", Plata = 33000 };
+            Radnik radnik4 = new Radnik { ime = "Jelena", prezime = "Antić", Adresa = "Bulevar Nemanjića 134", Plata = 40000, oznake = new List<string> { "žena", "pravnik" } };
+            Radnik radnik5 = new Radnik { ime = "Mirjana", prezime = "Vukić", Adresa = "Bulevar Nemanjića 23", Plata = 29000, oznake = new List<string> { "žena" } };
+
+            collection.Insert(radnik1);
+            collection.Insert(radnik2);
+            collection.Insert(radnik3);
+            collection.Insert(radnik4);
+            collection.Insert(radnik5);
+
+            foreach(Radnik radnik in collection.FindAll())
+            {
+                MessageBox.Show(radnik.ime);
+            }
+            
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //db.preduzece.update({ime:"Milutin"}, {$set:{oznake:["test"]}});
+
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var db = server.GetDatabase("preduzece");
+
+            var collection = db.GetCollection<Radnik>("radnici");
+
+            var query = Query.EQ("ime", "Milutin");
+            var update = MongoDB.Driver.Builders.Update.Set("oznake", BsonValue.Create(new List<string>{"test"}));
+
+            collection.Update(query, update);
+
+            foreach (Radnik r in collection.Find(Query.EQ("ime", BsonValue.Create("Milutin"))))
+            {
+                foreach (string oznaka in r.oznake)
+                {
+                    MessageBox.Show(r.ime + ":" + oznaka);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var db = server.GetDatabase("preduzece");
+
+            var collection = db.GetCollection<Radnik>("radnici");
+
+            Radnik testRadnik = new Radnik { ime = "Test", prezime = "Test", Adresa = "Test adresa", Plata = 1000};
+
+            collection.Insert(testRadnik);
+
+            foreach (Radnik radnik in collection.FindAll())
+            {
+                MessageBox.Show(radnik.ime);
+            }
+
+            var query = Query.EQ("ime", "Test");
+
+            collection.Remove(query);
+
+            foreach (Radnik radnik in collection.FindAll())
+            {
+                MessageBox.Show(radnik.ime);
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var db = server.GetDatabase("preduzece");
+
+            var collection = db.GetCollection<Radnik>("radnici");
+
+            MessageBox.Show("Broj radnika: " + collection.Count());
+
+            MessageBox.Show("Prva dva radnika");
+
+            foreach (Radnik r in collection.FindAll().SetLimit(2))
+            {
+                MessageBox.Show(r.ime + " " + r.prezime);
+            }
+
+
+            MessageBox.Show("3. i 4. radnik u kolekciji");
+
+            foreach (Radnik r in collection.FindAll().SetSkip(2).SetLimit(2))
+            {
+                MessageBox.Show(r.ime + " " + r.prezime);
+            }
+
+            MessageBox.Show("Radnici sortirani po prezimenu");
+
+            foreach (Radnik r in collection.FindAll().SetSortOrder(new string[] { "prezime" })) //ILI: //collection.FindAll().SetSortOrder(SortBy.Descending("prezime"));
+            {
+                MessageBox.Show(r.ime + " " + r.prezime);
+            }
+
+            MessageBox.Show("Radnici koji se ne zovu Jelena i imaju platu manju od 40000");
+
+            var query = Query.And(
+                            Query.Not(Query.EQ("ime", "Jelena")), 
+                            Query.LT("Plata", 40000)
+                            );
+
+            foreach (Radnik r in collection.Find(query))
+            {
+                MessageBox.Show(r.ime + " " + r.prezime);
+            }
+
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var db = server.GetDatabase("preduzece");
+
+            var radniciCollection = db.GetCollection<Radnik>("radnici");
+
+            var sektoriCollection = db.GetCollection<Sektor>("sektori");
+
+            Sektor sek = new Sektor{Naziv="sektor 1"};
+            sektoriCollection.Insert(sek);
+
+            //svi radnici osim prva 2 rade u sektoru 1
+            foreach (Radnik r in radniciCollection.FindAll().SetSkip(2))
+            {
+                sek.Radnici.Add(new MongoDBRef("radnici", r.Id));
+                
+                r.Sektor = new MongoDBRef("sektori", sek.Id);
+
+                radniciCollection.Save(r);
+            }
+
+            sektoriCollection.Save(sek);
+
+            foreach (Sektor s in sektoriCollection.FindAll())
+            {
+                foreach (MongoDBRef radnikRef in s.Radnici.ToList())
+                {
+                    Radnik radnik = db.FetchDBRefAs<Radnik>(radnikRef);
+                    MessageBox.Show(radnik.ime + " " + radnik.prezime + " sektor: " + s.Naziv);
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var db = server.GetDatabase("preduzece");
+
+            var sektoriCollection = db.GetCollection<Sektor>("sektori");
+            sektoriCollection.RemoveAll();
+
+            //MessageBox.Show("");
+            var radnici = db.GetCollection<Radnik>("radnici");
+            radnici.RemoveAll();
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var db = server.GetDatabase("preduzece");
+
+            var radniciCollection = db.GetCollection("radnici");
+
+            var sektoriCollection = db.GetCollection("sektori");
+
+            
+            //simple
+            var query1 = from radnik in radniciCollection.AsQueryable<Radnik>()
+                        where radnik.ime == "Petar"
+                        select radnik;
+            
+            foreach (Radnik r in query1)
+            {
+                MessageBox.Show(r.ime + " " + r.prezime);
+            }
+
+            //simple count
+            var result1 = (from radnik in radniciCollection.AsQueryable<Radnik>()
+                         select radnik).Count();
+
+            MessageBox.Show("Broj radnika: " + result1.ToString());
+
+            //simple
+            var result2 = (from radnik in radniciCollection.AsQueryable<Radnik>()
+                           where radnik.Plata > 20000
+                           orderby radnik.prezime descending 
+                           select radnik).FirstOrDefault();
+            MessageBox.Show("Prvi radnik po opadajucem redosledu po prezimenu koji ima platu vecu od 20000: " + result2.ime + " " + result2.prezime);
+
+            
+        }
+    }
+}
---
+++ MongoDB_WebShop/App_Code/Service.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Text;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+public class Service : IService
+{
+
+    public string[] GetProductList(int page)
+    {
+        //return new string[] {"{\"_id\":1, \"name\":\"Proizvod 1\", \"imageURL\":\"1.jpg\"}",
+        //                    "{\"_id\":2, \"name\":\"Proizvod 2\", \"imageURL\":\"2.jpg\"}",
+        //                    "{\"_id\":3, \"name\":\"Proizvod 3\", \"imageURL\":\"3.jpg\"}",
+        //                    "{\"_id\":4, \"name\":\"Proizvod 4\", \"imageURL\":\"4.jpg\"}",
+        //                    "{\"_id\":5, \"name\":\"Proizvod 5\", \"imageURL\":\"5.jpg\"}",
+        //                    "{\"_id\":6, \"name\":\"Proizvod 6\", \"imageURL\":\"6.jpg\"}",
+        //                    "{\"_id\":7, \"name\":\"Proizvod 7\", \"imageURL\":\"7.jpg\"}",
+        //                    "{\"_id\":8, \"name\":\"Proizvod 8\", \"imageURL\":\"8.jpg\"}",
+        //                    "{\"_id\":9, \"name\":\"Proizvod 9\", \"imageURL\":\"9.jpg\"}"};
+
+        var connectionString = "mongodb://localhost/?safe=true";
+        var server = MongoServer.Create(connectionString);
+        var db = server.GetDatabase("products");
+
+        var collection = db.GetCollection<BsonDocument>("products");
+
+        List<string> products = new List<string>();
+
+        foreach (BsonDocument doc in collection.FindAll())
+        {
+            products.Add(doc.ToJson());
+        }
+
+        return products.ToArray();
+                             
+    }
+
+
+    public string GetProductDetails(string id)
+    {
+        return string.Empty;
+    }
+}
